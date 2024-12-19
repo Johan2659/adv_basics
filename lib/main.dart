740d7038120304e47338465d69517fb1d9be3f54
YOUR_NAME <tosojohan@gmail.com>
2024-12-18 19:50:50 -0700

    Background + gradient orange added

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,22 @@ import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFFFFCC80), // Début du gradient (orange clair)
+                Color(0xFFFFA726), // Milieu du gradient (orange moyen)
+                Color(0xFFE65100), // Fin du gradient (orange très foncé)
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
